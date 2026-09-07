0736208efcc0afa82f2d623b8a4473991783c469
edudole <mintanaeducation@gmail.com>
2026-09-07 11:15:04 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbxba2Enfzaz4wu7supLpkW2V-3aQLk4AG6KtPjc6GczcifG4J7wXpcE85pmAZsoKXdBhg/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbw7gUWih1UGW37lxpi1jj98G0k35iVN7FVRMP-E4HeMs0ea54oTtAI9f2uj4OYHPx4w/exec'
   });
 })();

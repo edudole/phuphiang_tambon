f363e74960592f69c41455790c47729c18688b63
edudole <mintanaeducation@gmail.com>
2026-09-12 01:20:02 +0700

    Update index-fast.js

--- index-fast.js
+++ index-fast.js
@@ -3076,7 +3076,7 @@ window.STUDENT_PROFILE_WEB_APP_URL =
   'use strict';
 
   const CLIPROOM_WEB_APP_URL =
-    'https://script.google.com/macros/s/AKfycbz6rNShJFZOlNmVb1ev7lzikFvxyhh2PohGsIjDnqDGRUyPunuw4TlsfWihigms-_YwLA/exec';
+    'https://script.google.com/macros/s/AKfycbwMYlQQN9RXTBwyCelTwqqXl1tIsxDfWfbi3mJsqaZEF5uRIDpkRTx7fstze6eA96fvAQ/exec';
   const CACHE_KEY = 'SITE_FAST:cliproom-catalog-v1';
   const CACHE_AGE = 5 * 60 * 1000;
   const track = document.getElementById('cliproomTrack');

05e8b8f542a8bae79c8b1afe7df3eb9ac2584529
edudole <mintanaeducation@gmail.com>
2026-09-16 00:34:20 +0700

    Update index-fast.js

--- index-fast.js
+++ index-fast.js
@@ -1686,7 +1686,7 @@ if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',
 
 /* ===== profile-config.js ===== */
 window.STUDENT_PROFILE_WEB_APP_URL =
-  '';
+  'https://script.google.com/macros/s/AKfycby7DjChYbHHeFr2aiPFzORgzGpTcsWmkyo80g7RXc3Vfmn7rV7lN5QhORBAgQpSNRmg/exec';
 
 ;
 
